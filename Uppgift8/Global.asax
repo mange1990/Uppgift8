6a3d14482ab3ba4c11379da3540974447ebf7806
magnus <magnus.rendahl@gmail.com>
2020-04-20 09:50:22 +0200

    * .gitignore: Init commit
    * Uppgift8.sln:
    * Web.config:
    * custom.css:
    * index.html:
    * Global.asax:
    * om oss.html:
    * Global.asax.cs:
    * produkter.html:
    * Uppgift8.csproj:

---
+++ Uppgift8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Uppgift8.Global" %>
---
+++ Uppgift8/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace Uppgift8
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
